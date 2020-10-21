2ff5083538312e086204902a14383e3481f576e0
Andrea Pullen <apullenb@gmail.com>
2020-10-21 10:32:56 -0500

    initial

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('common'));
+const plays = require('./playstore.js')
+
+app.get('/apps', (req, res) => {
+    let returnData = [...plays]
+    const {search} = req.query;
+    returnData = returnData.filter(play => play.App.toLowerCase().includes(search.toLowerCase()));
+    res
+        .json(returnData);
+});
+
 
 app.listen(8080, () => console.log('server started'));
